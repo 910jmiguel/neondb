ff590b94ef1c1e4b56d2dbe9c0bc71a43c998811
910jmiguel <910jmiguel@gmail.com>
2025-03-20 17:16:04 -0400

    Add Prisma setup and Expense model with CRUD operations

--- app/page.tsx
+++ app/page.tsx
@@ -1,8 +1,11 @@
 export default function Home() {
+// fetch expenses from the server
+
   return (
     <section className='pb-24 pt-40'>
       <div className='container'>
-        <h1 className='text-2xl font-semibold'>Next Shadcn Template</h1>
+        <h1 className="text-3xl font-bold">Expense Tracker</h1>
+        <h2 className="text-zinc-500">Using Neon: Serverless Postgres</h2>
       </div>
     </section>
   )
